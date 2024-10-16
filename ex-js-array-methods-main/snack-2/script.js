9a6f56c2246b806f7b159946dcd994d4a13edd0e
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-16 15:43:56 +0200

    2nd done

---
+++ ex-js-array-methods-main/snack-2/script.js
@@ -0,0 +1,14 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
+for (let i = 0; i < people.length; i++) {
+  const element = people[i];
+  console.log(element.name);
+  
+  
+}
